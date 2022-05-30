ace4ffe539fb3e172c3d8eaac898c171aaf3c9ef
Thu Mai <mnathu98@gmail.com>
2022-05-30 02:38:34 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,103 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import "./App.css";
+import { useState } from "react";
+import { FaHandRock, FaHandPaper, FaHandScissors } from "react-icons/fa";
+const actions = {
+  rock: "scissors",
+  paper: "rock",
+  scissors: "paper",
+};
+const randomAction = () => {
+  // const actions = {
+  //   rock: "scissors",
+  //   paper: "rock",
+  //   scissors: "paper",
+  // };
+  const keys = Object.keys(actions);
+  const index = Math.floor(Math.random() * keys.length);
+  return keys[index];
+};
+const ActionIcon = ({ action, ...props }) => {
+  const icons = {
+    rock: FaHandRock,
+    paper: FaHandPaper,
+    scissors: FaHandScissors,
+  };
+  const Icon = icons[action];
+  return <Icon {...props} />;
+};
+const Player = ({ name = "Player", score = 0, action = "rock" }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="player">
+      <div className="score">{`${name}: ${score}`}</div>
+      <div className="action">
+        <ActionIcon action={action} size={60} />
+      </div>
+    </div>
+  );
+};
+const calculateWinner = (action1, action2) => {
+  if (action1 === action2) {
+    return 0;
+  } else if (actions[action1] === action2) {
+    return -1;
+  } else if (actions[action2] === action1) {
+    return 1;
+  }
+  //this should be never happened
+  return null;
+};
+const ActionButton = ({ action = "rock", onActionSelected }) => {
+  return (
+    <button className="round-btn" onClick={() => onActionSelected(action)}>
+      <ActionIcon action={action} size={20} />
+    </button>
+  );
+};
+const ShowWinner = ({ winner = 0 }) => {
+  const text = {
+    "-1": "You Win!",
+    0: "It's a Tie",
+    1: "You Lose!",
+  };
+  return (<h2>{text[winner]}</h2>);
+};
+function App() {
+  const [playerAction, setPlayerAction] = useState("");
+  const [computerAction, setComputerAction] = useState("");
+  const [playerScore, setPlayerScore] = useState(0);
+  const [computerScore, setComputerScore] = useState(0);
+  const [winner, setWinner] = useState(0);
+  const onActionSelected = (selectedAction) => {
+    const newComputerAction = randomAction();
+    setPlayerAction(selectedAction);
+    setComputerAction(newComputerAction);
+    const newWinner = calculateWinner(selectedAction, newComputerAction);
+    setWinner(newWinner);
+    if (newWinner === -1) {
+      setPlayerScore(playerScore + 1);
+    } else if (newWinner === 1) {
+      setComputerScore(computerScore + 1);
+    }
+  };
+  return (
+    <div className="center">
+      <h1>Rock Paper Scissors</h1>
+      <div>
+        <div className="container">
+          <Player name="Player" score={playerScore} action={playerAction} />
+          <Player
+            name="Computer"
+            score={computerScore}
+            action={computerAction}
+          />
+        </div>
+        <div>
+          <ActionButton action="rock" onActionSelected={onActionSelected} />
+          <ActionButton action="paper" onActionSelected={onActionSelected} />
+          <ActionButton action="scissors" onActionSelected={onActionSelected} />
+        </div>
+        <ShowWinner winner={winner} />
+      </div>
     </div>
   );
 }
